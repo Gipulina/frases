80e4e72c69e235dd734582ac360ca330edefc069
Gipulina <georgi_palavecino@hotmail.com>
2021-07-02 11:28:55 -0300

    adding the phrases to the page with Js scripts

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const twitterBtn = document.getElementById('twitter');
+const authorText = document.getElementById('author');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
 let apiQuotes = [];
 
 //Show Quote
 //pick a random quote fro apiQuotes array
 function newQuote() {
-  const Quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];    
-  console.log(Quote);
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];    
+  authorText.textContent = quote.author;
+  quoteText.textContent = quote.text;
 }
 
 //Get Quotes from API
@@ -13,8 +21,8 @@ async function getQuotes() {
     try{
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
-        newQote();
-        //console.log(apiQuotes[12]); esta es para seleccionra una específica
+        newQuote();
+    //console.log(apiQuotes[12]); esta es para seleccionra una específica
     }catch(error){
         //Catch Error Here
         // alert("Something worked wrong");
