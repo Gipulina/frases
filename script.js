acbee28fdd47beacbeb64c680edebab50d56059b
Gipulina <georgi_palavecino@hotmail.com>
2021-07-02 01:37:55 -0300

    Javascript Random Seleccion of the phrases

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// Uso esto para chequear que esté conectada la hoja JS al HTML console.log('testing'); 
+let apiQuotes = [];
+
+//Show Quote
+//pick a random quote fro apiQuotes array
+function newQuote() {
+  const Quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];    
+  console.log(Quote);
+}
+
+//Get Quotes from API
+async function getQuotes() {
+    const apiUrl="https://type.fit/api/quotes";
+    try{
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQote();
+        //console.log(apiQuotes[12]); esta es para seleccionra una específica
+    }catch(error){
+        //Catch Error Here
+        // alert("Something worked wrong");
+    }
+}
+
+// On Load
+getQuotes();
