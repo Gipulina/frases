9ebb290c622797176f6e2190b28d5f1b3feba6b2
Gipulina <georgi_palavecino@hotmail.com>
2021-07-03 03:25:02 -0300

    loader added

--- script.js
+++ script.js
@@ -3,13 +3,26 @@ const quoteText = document.getElementById('quote');
 const twitterBtn = document.getElementById('twitter');
 const authorText = document.getElementById('author');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
 let apiQuotes = [];
 
+//show loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+//hide loading 
+function complete(){
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
 //Show Quote
 //pick a random quote fro apiQuotes array
 function newQuote() {
+   loading();
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];    
   //check if the author is blank
   if(!quote.author) {
@@ -29,10 +42,13 @@ function newQuote() {
         quoteText.classList.remove('long-quote');
   }
   quoteText.textContent = quote.text;
+  //Set Quote, hidden loader
+  complete();
 }
 
 //Get Quotes from API
 async function getQuotes() {
+    loading();
     const apiUrl="https://type.fit/api/quotes";
     try{
         const response = await fetch(apiUrl);
